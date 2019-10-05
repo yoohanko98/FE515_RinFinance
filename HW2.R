b4536c99dbee86559dfbab80c95b47c53d938782
Yoohan Ko <yoohanko98@gmail.com>
2019-10-05 17:40:42 -0400

    working on part 2

--- HW2.R
+++ HW2.R
@@ -9,6 +9,8 @@
 # 1. Explain what is dgeom() and pgeom(). 
   # Create a simple question by yourself and answer it with these two functions
 
+# the dgenom function calculates the probability mass function (p.m.f.) of a geometric distribution
+
 
 # 2. Explain what is qgeom(). Plug-in two numbers and explain the answer.
 
@@ -27,11 +29,33 @@
   # Trade data shows the historical transaction price. 
   # Quote data shows the desired price from traders.
 
+# Importing data set provided
+# setwd(paste("/Users/yoohanko98/OneDrive - stevens.edu/Stevens/Semester 7/
+#             FE 515 (R in Finance)/HW/FE515_RinFinance/HW2", sep=""))
+BA = read.csv("HW2/BA.csv", header = TRUE)
+is(BA)
+head(BA)
 
 # 1. Use subset() function to get a sub-table which contains only “Quote” data.
 
+?subset
+# https://www.r-bloggers.com/5-ways-to-subset-a-data-frame-in-r/
+BA.quote <- subset(BA, Type == "Quote")
+head(BA.quote)
+
 # 2. Combine the “Date.L.” column and “Time.L.” column. 
   # Then, use strptime() to convert it into a time object.
+
+# paste is used to combine two character strings
+BA.quote$combinedtime <- paste(BA.quote$Date.L., BA.quote$Time.L.)
+
+?strptime
+head(BA.quote$combinedtime)
+
+# set the number of digits in seconds to 6 to not lose any information
+options(digits.secs = 6)
+
+BA.quote$combinedtime <- strptime(BA.quote$combinedtime, "%Y%m%d %H:%M:%OS")
 
 
 # 3. Find out the last bid price and ask price in each minute. 
@@ -50,6 +74,10 @@
   # Now, I will give you one of the box randomly and you need to take one marble outside the box. What is the probability that the marble is a black one?
   # Please simulate this game for 1000 times and calculate the probability based on your observa- tions.
 
+
+
+
+# Bonus Question (15 points)
 # • Calculate theoretical value of the probability that the marble is a black one based on the setting from Question 3. (5 points)
 
 
